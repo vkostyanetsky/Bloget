5e48462028a10f837c4586fe5eb80f792428d833
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2022-05-22 15:53:08 +0700

    Implemented BlogBuilderPaths

--- builder2.py
+++ builder2.py
@@ -8,158 +8,173 @@ from jinja2 import Environment
 from jinja2 import FileSystemLoader
 
 
-class BuilderPaths:
-    __builder_path: str
-    __input_directory_path: str
-    __output_directory_path: str
-
-    def __init__(self, input_directory_path: str, output_directory_path: str):
-        self.__builder_path = os.path.abspath(os.path.dirname(__file__))
-        self.__input_directory_path = input_directory_path
-        self.__output_directory_path = output_directory_path
-
-    def get_input_pages_directory_path(self) -> str:
-        return os.path.join(self.__input_directory_path, 'pages')
-
-    def get_input_notes_pages_directory_path(self) -> str:
-        return os.path.join(
-            self.get_input_pages_directory_path(),
-            'notes'
-        )
-
-    def get_input_tags_file_path(self) -> str:
-        return os.path.join(self.__input_directory_path, 'tags.yaml')
-
-    def get_input_language_file_path(self) -> str:
-        return os.path.join(self.__input_directory_path, 'language.yaml')
-
-    def get_output_directory_path(self) -> str:
-        return self.__output_directory_path
-
-    def get_output_notes_directory_path(self) -> str:
-        return os.path.join(self.__output_directory_path, 'notes')
-
-    def get_builder_templates_directory_path(self) -> str:
-        return os.path.join(self.__builder_path, 'templates')
-
-
-class BuilderConfig:
-    __config: dict
-
-    def __init__(self, config_path: str):
-        self.__config = Builder.read_yaml_file(
-            file_path=config_path
-        )
-
-
-class BlogPage:
-    __attachments: list = []
-    # directory_path: str
-    # directory_name: str
-    __input_directory_path: str = ''
-    __output_directory_path: str = ''
-
-    __paths: BuilderPaths = None
-    __config: BuilderConfig = None
-
-    __path: str
-    __date: str
-
-    __predefined_file_names: list = ['index.md', 'index.yaml']
-
-    def __init__(self, input_directory_path: str, paths: BuilderPaths, config: BuilderConfig):
-        self.__input_directory_path = input_directory_path
-
-        self.output_directory_path = input_directory_path.replace(
-            paths.get_input_pages_directory_path(),
-            paths.get_output_directory_path()
-        )
-
-        self.__paths = paths
-        self.__config = config
-
-        self.__set_path()
-        self.__set_attachments()
-
-    def __set_path(self):
-
-        self.__path = self.__input_directory_path
-
-        directories = []
-
-        while self.__path != self.__paths.get_input_pages_directory_path():
-            split_path = os.path.split(self.__path)
-            self.__path = split_path[0]
-
-            directories.append(split_path[1])
-
-        directories = list(reversed(directories))
-
-        if len(directories) > 0:
-            self.__path = '/{}/'.format('/'.join(directories))
-        else:
-            self.__path = '/'
-
-    def __set_attachments(self):
-
-        file_names = os.listdir(self.__input_directory_path)
-
-        for file_name in file_names:
-
-            file_path = os.path.join(self.__input_directory_path, file_name)
-
-            if os.path.isfile(file_path) and file_name not in self.__predefined_file_names:
-                self.__attachments.append(file_name)
-
-    @staticmethod
-    def is_page(page_directory_path) -> bool:
-        return os.path.exists(
-            os.path.join(page_directory_path, 'index.yaml')
-        )
+# class BlogPage:
+#     __attachments: list = []
+#     __input_directory_path: str = ''
+#     __output_directory_path: str = ''
+#
+#     __path: str
+#     __date: str
+#
+#     __predefined_file_names: list = ['index.md', 'index.yaml']
+#
+#     def __init__(self, input_directory_path: str, paths: BuilderPaths, config: BuilderConfig):
+#         self.__input_directory_path = input_directory_path
+#
+#         self.output_directory_path = input_directory_path.replace(
+#             paths.get_input_pages_directory_path(),
+#             paths.get_output_directory_path()
+#         )
+#
+#         self.__paths = paths
+#         self.__config = config
+#
+#         self.__set_path()
+#         self.__set_attachments()
+#
+#     def __set_path(self):
+#
+#         self.__path = self.__input_directory_path
+#
+#         directories = []
+#
+#         while self.__path != self.__paths.get_input_pages_directory_path():
+#             split_path = os.path.split(self.__path)
+#             self.__path = split_path[0]
+#
+#             directories.append(split_path[1])
+#
+#         directories = list(reversed(directories))
+#
+#         if len(directories) > 0:
+#             self.__path = '/{}/'.format('/'.join(directories))
+#         else:
+#             self.__path = '/'
+#
+#     def __set_attachments(self):
+#
+#         file_names = os.listdir(self.__input_directory_path)
+#
+#         for file_name in file_names:
+#
+#             file_path = os.path.join(self.__input_directory_path, file_name)
+#
+#             if os.path.isfile(file_path) and file_name not in self.__predefined_file_names:
+#                 self.__attachments.append(file_name)
 
 
-class Builder:
-    __paths: BuilderPaths
-    __config: BuilderConfig
+# class BlogPageBuilder:
+#     __paths: BuilderPaths = None
+#     __config: BuilderConfig = None
+#
+#     @staticmethod
+#     def is_page(page_directory_path) -> bool:
+#         return os.path.exists(
+#             os.path.join(page_directory_path, 'index.yaml')
+#         )
 
+class BlogBuilderPaths:
+
+    __application_path: str = ''
+    __templates_path: str = ''
+
+    __input_path: str = ''
+    __input_tags_path: str = ''
+    __input_language_path: str = ''
+    __input_pages_path: str = ''
+    __input_notes_path: str = ''
+
+    __output_path: str = ''
+    __output_notes_path: str = ''
+
+    def __init__(self, input_path: str, output_path: str):
+        self.__application_path = os.path.abspath(os.path.dirname(__file__))
+        self.__templates_path = os.path.join(self.__application_path, 'templates')
+
+        self.__input_path = input_path
+        self.__input_tags_path = os.path.join(self.__input_path, 'tags.yaml')
+        self.__input_language_path = os.path.join(self.__input_path, 'language.yaml')
+        self.__input_pages_path = os.path.join(self.__input_path, 'pages')
+        self.__input_notes_path = os.path.join(self.__input_pages_path, 'notes')
+
+        self.__output_path = output_path
+        self.__output_notes_path = os.path.join(self.__output_path, 'notes')
+
+    @property
+    def application_path(self) -> str:
+        return self.__application_path
+
+    @property
+    def templates_path(self) -> str:
+        return self.__templates_path
+
+    @property
+    def input_path(self) -> str:
+        return self.__input_path
+
+    @property
+    def input_tags_path(self) -> str:
+        return self.__input_tags_path
+
+    @property
+    def input_language_path(self) -> str:
+        return self.__input_language_path
+
+    @property
+    def input_pages_path(self) -> str:
+        return self.__input_pages_path
+
+    @property
+    def input_notes_path(self) -> str:
+        return self.__input_notes_path
+
+    @property
+    def output_path(self) -> str:
+        return self.__output_path
+
+    @property
+    def output_notes_path(self) -> str:
+        return self.__output_notes_path
+
+
+class BlogBuilderApplication:
+    __config: dict = {}
+    __paths: BlogBuilderPaths
+    __language: dict = {}
+    __tags: dict = {}
     __templates: Environment
-    __language: dict
-    __tags: dict
-
     __notes: list = []
     __texts: list = []
 
-    def __init__(self, config_path: str, input_directory_path: str, output_directory_path: str):
-        self.__paths = BuilderPaths(
-            input_directory_path=input_directory_path,
-            output_directory_path=output_directory_path
-        )
+    def __init__(self, input_path: str, output_path: str, config_path: str) -> None:
 
-        self.__config = BuilderConfig(
-            config_path=config_path
-        )
+        self.__paths = BlogBuilderPaths(input_path=input_path, output_path=output_path)
 
-    def run(self):
-        self.__clear_output_directory()
+        self.__set_config(config_path)
+        self.__set_language()
+        self.__set_tags()
+        self.__set_templates()
 
+    def __set_config(self, config_path: str) -> None:
+        self.__config = self.read_yaml_file(file_path=config_path)
+
+    def __set_language(self) -> None:
+        self.__language = self.read_yaml_file(file_path=self.__paths.input_language_path)
+
+    def __set_tags(self) -> None:
+        self.__tags = self.read_yaml_file(file_path=self.__paths.input_tags_path)
+
+    def __set_templates(self) -> None:
         self.__templates = Environment(
-            loader=FileSystemLoader(
-                searchpath=self.__paths.get_builder_templates_directory_path()
-            )
+            loader=FileSystemLoader(searchpath=self.__paths.templates_path)
         )
 
-        self.__language = self.read_yaml_file(
-            file_path=self.__paths.get_input_language_file_path()
-        )
-
-        self.__tags = self.read_yaml_file(
-            file_path=self.__paths.get_input_tags_file_path()
-        )
-
+    def run(self) -> None:
+        self.__clear_output_directory()
         self.__load_pages()
 
-    def __clear_output_directory(self):
-        output_directory = self.__paths.get_output_directory_path()
+    def __clear_output_directory(self) -> None:
+        output_directory = self.__paths.output_path
 
         for project_file in os.listdir(output_directory):
 
@@ -174,33 +189,31 @@ class Builder:
                 elif os.path.isdir(project_file_path):
                     shutil.rmtree(project_file_path)
 
-    def __load_pages(self):
+    def __load_pages(self) -> None:
 
-        notes_directory_path = self.__paths.get_input_notes_pages_directory_path()
+        notes_path = self.__paths.input_notes_path
 
-        directories = os.walk(
-            self.__paths.get_input_pages_directory_path()
-        )
+        directories = os.walk(self.__paths.input_pages_path)
 
         for directory in directories:
 
             directory_path = directory[0]
 
-            if not BlogPage.is_page(directory_path):
-                continue
+            # if not BlogPage.is_page(directory_path):
+            #     continue
 
-            is_note = directory_path.startswith(notes_directory_path)
+            # is_note = directory_path.startswith(notes_directory_path)
 
-            page = BlogPage(
-                input_directory_path=directory_path,
-                paths=self.__paths,
-                config=self.__config
-            )
-
-            if is_note:
-                self.__notes.append(page)
-            else:
-                self.__texts.append(page)
+            # page = BlogPage(
+            #     input_directory_path=directory_path,
+            #     paths=self.__paths,
+            #     config=self.__config
+            # )
+            #
+            # if is_note:
+            #     self.__notes.append(page)
+            # else:
+            #     self.__texts.append(page)
 
         # self.__notes = sorted(
         #     self.__notes,
@@ -245,8 +258,8 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
-    Builder(
-        config_path=args.config,
-        input_directory_path=args.input,
-        output_directory_path=args.output
+    BlogBuilderApplication(
+        input_path=args.input,
+        output_path=args.output,
+        config_path=args.config
     ).run()
