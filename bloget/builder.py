2570a139a17297807ffc3628b55b18b2e9f8e652
vkostyanetsky <vlad@kostyanetsky.ru>
2022-12-03 23:24:30 +0400

    Robots.txt writer

--- bloget/builder.py
+++ bloget/builder.py
@@ -12,7 +12,7 @@ import shutil
 
 from bloget import utils, webserver
 from bloget.readers import metadata_reader, pages_reader
-from bloget.writers import page_404_writer, rss_feed_writer, sitemap_writer, text_writer
+from bloget.writers import page_404_writer, rss_feed_writer, sitemap_writer, text_writer, robots_writer
 
 
 def build_blog(arguments: argparse.Namespace) -> None:
@@ -31,6 +31,7 @@ def build_blog(arguments: argparse.Namespace) -> None:
     sitemap_writer.write_sitemap(pages, metadata)
     rss_feed_writer.write_rss_feed(pages, metadata)
     page_404_writer.write_page_404(metadata)
+    robots_writer.write_robots(metadata)
 
     __copy_skin_assets(metadata)
 
---
+++ bloget/writers/robots_writer.py
@@ -0,0 +1,34 @@
+import logging
+import os
+
+from bloget import utils
+from bloget.readers import metadata_reader
+
+
+def write_robots(metadata: metadata_reader.BlogMetadata) -> None:
+    """
+    Builds & writes the robots.txt file.
+    """
+
+    logging.info("Robots building...")
+
+    file_text = __get_file_text(metadata)
+    file_path = os.path.join(metadata.paths["output"], "robots.txt")
+
+    utils.make_file(file_path, file_text)
+
+    logging.info("Robots building has been done!")
+
+
+def __get_file_text(
+    metadata: metadata_reader.BlogMetadata
+) -> str:
+    """
+    Returns content of the robots.txt file.
+    """
+
+    template_parameters = {
+        "settings": metadata.settings
+    }
+
+    return metadata.templates.get_template("robots.txt").render(template_parameters)
