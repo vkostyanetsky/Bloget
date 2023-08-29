ec45f9a6f3c328a1769214a2817f0f4b4f466246
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2023-08-29 23:57:13 +0400

    wtf

---
+++ bloget/writers/notes_list_writer.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python3
+
+"""
+Implementation of note pages building functionality.
+"""
+
+import logging
+import os
+import typing
+
+from bloget import constants, utils
+from bloget.readers import metadata_reader, page_reader, pages_reader
+from bloget.writers.utils import page_writing_utils
+
+
+def write_note_lists(
+    pages: pages_reader.BlogPages, metadata: metadata_reader.BlogMetadata
+) -> None:
+    """
+    Builds note list pages.
+    """
+
+    __write_note_lists_by_selected_tag(
+        pages=pages, selected_tag=None, metadata=metadata
+    )
+
+    for selected_tag in metadata.tags:
+        __write_note_lists_by_selected_tag(
+            pages=pages, selected_tag=selected_tag, metadata=metadata
+        )
+
+
+def __write_note_lists_by_selected_tag(
+    pages: pages_reader.BlogPages,
+    selected_tag: str | None,
+    metadata: metadata_reader.BlogMetadata,
+) -> None:
+    """
+    Writes note list pages by selected tag.
+    """
+
+    tag_comment = page_writing_utils.get_selected_tag_comment(selected_tag)
+    logging.info("Note lists building %s", tag_comment)
+
+    notes = page_writing_utils.get_notes_by_tag(pages.notes, selected_tag)
+    notes_left = len(notes)
+
+    list_number = 1
+    list_notes = []
+    list_size = 20
+
+    for note in notes:
+        list_notes.append(note)
+        notes_left -= 1
+
+        if len(list_notes) == list_size or notes_left == 0:
+            list_is_last = notes_left == 0
+
+            __write_notes_list(
+                list_notes=list_notes,
+                list_number=list_number,
+                list_is_last=list_is_last,
+                selected_tag=selected_tag,
+                metadata=metadata,
+            )
+
+            list_number += 1
+            list_notes = []
+
+
+def __get_note_list_folder_path(
+    list_number: int, selected_tag: str | None, metadata: metadata_reader.BlogMetadata
+) -> str:
+    """
+    Returns a path to note list folder.
+
+    For instance: D:/Blog/notes/tags/alice/page-1
+    """
+
+    result = os.path.join(metadata.paths["output"], constants.NOTES_FOLDER_NAME)
+
+    if selected_tag is not None:
+        result = os.path.join(result, "tags", selected_tag)
+
+    if list_number > 1:
+        result = os.path.join(result, f"page-{list_number}")
+
+    return result
+
+
+def __write_notes_list(
+    list_notes: list[page_reader.BlogPage],
+    list_number: int,
+    list_is_last: bool,
+    selected_tag: str | None,
+    metadata: metadata_reader.BlogMetadata,
+) -> None:
+    """
+    Writes a note list.
+
+    Without a selected tag:
+        notes/index.html
+        notes/page-1/index.html
+
+    With a selected tag "alice":
+        notes/tags/alice/index.html
+        notes/tags/alice/page-1/index.html
+    """
+
+    folder_path = __get_note_list_folder_path(list_number, selected_tag, metadata)
+
+    file_text = __get_notes_list_file_text(
+        list_notes, list_number, list_is_last, selected_tag, metadata
+    )
+    file_path = os.path.join(folder_path, "index.html")
+
+    utils.make_folder(folder_path)
+    utils.make_file(file_path, file_text)
+
+
+def __get_notes_list_file_text(
+    list_notes: list[page_reader.BlogPage],
+    list_number: int,
+    list_is_last: bool,
+    selected_tag: str | None,
+    metadata: metadata_reader.BlogMetadata,
+) -> str:
+    """
+    Returns template parameters for the note.html file.
+    """
+
+    template_parameters = __get_note_list_template_parameters(
+        list_notes, list_number, list_is_last, selected_tag, metadata
+    )
+
+    return metadata.templates.get_template("notes_list.html").render(
+        template_parameters
+    )
+
+
+def __get_note_list_page_title(
+    selected_tag: str | None, metadata: metadata_reader.BlogMetadata
+) -> str:
+    """
+    Returns a title of a note list.
+
+    For instance,
+        Notes (no tag selected)
+        Alice ("alice" tag selected whose title is "alice")
+    """
+
+    if selected_tag is None:
+        result = metadata.language["notes"]
+    else:
+        result = (
+            metadata.tags[selected_tag][:1].upper() + metadata.tags[selected_tag][1:]
+        )
+
+    return result
+
+
+def __get_note_list_page_path(list_number: int, selected_tag: str | None) -> str:
+    """
+    Returns a path of a note list.
+
+    For instance,
+        notes
+        notes/page-1
+        notes/tags/alice/
+        notes/tags/alice/page-1
+    """
+
+    path_parts = [constants.NOTES_FOLDER_NAME]
+
+    if selected_tag is not None:
+        path_parts.append("tags")
+        path_parts.append(selected_tag)
+
+    if list_number > 1:
+        path_parts.append(f"page-{list_number}")
+
+    return "/".join(path_parts)
+
+
+def __get_note_list_page_url(
+    list_number: int, selected_tag: str | None, metadata: metadata_reader.BlogMetadata
+) -> str:
+    url_parts = metadata.settings["url"]
+    page_path = __get_note_list_page_path(list_number, selected_tag)
+
+    return f"{url_parts}/{page_path}"
+
+
+def __get_note_list_template_parameters(
+    list_notes: list[page_reader.BlogPage],
+    list_number: int,
+    list_is_last: bool,
+    selected_tag: str | None,
+    metadata: metadata_reader.BlogMetadata,
+) -> dict[str, typing.Any]:
+    page_title = __get_note_list_page_title(selected_tag, metadata)
+    page_path = __get_note_list_page_path(0, selected_tag)
+
+    result = page_writing_utils.get_html_template_parameters(
+        metadata=metadata,
+        page_title=page_title,
+        page_description="",
+        page_path=page_path,
+        page_is_editable=False,
+    )
+
+    result["selected_tag"] = selected_tag
+    result["tags"] = metadata.tags
+    result["notes"] = list_notes
+
+    if list_number > 1:
+        next_list_url = __get_note_list_page_url(
+            list_number - 1, selected_tag, metadata
+        )
+
+        result["next_list_url"] = next_list_url
+        result["hotkey_ctrl_up_url"] = next_list_url
+
+    if not list_is_last:
+        previous_list_url = __get_note_list_page_url(
+            list_number + 1, selected_tag, metadata
+        )
+
+        result["previous_list_url"] = previous_list_url
+        result["hotkey_ctrl_down_url"] = previous_list_url
+
+    return result
---
+++ bloget/writers/utils/page_writing_utils.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+"""
+Common methods which are applicable to both types of pages (texts & notes).
+"""
+
+import logging
+import os
+import typing
+
+from bloget import utils
+from bloget.readers import metadata_reader, page_reader
+
+
+def get_notes_by_tag(
+    notes: list[page_reader.BlogPage], tag: str | None
+) -> list[page_reader.BlogPage]:
+    """
+    Filters notes list, then sorts it.
+    """
+
+    notes = [note for note in notes if tag is None or tag in note.tags]
+
+    return sorted(notes, key=lambda note: note.created, reverse=True)
+
+
+def copy_page_attachments(page: page_reader.BlogPage, output_folder_path: str) -> None:
+    """
+    Copies page's attachments to the page build folder.
+    """
+
+    if page.attachments:
+        for attachment in page.attachments:
+            logging.debug('Copying attachment from "%s"', attachment)
+
+            source_file_path = os.path.join(page.folder_path, attachment)
+            target_file_path = os.path.join(output_folder_path, attachment)
+
+            utils.copy_file(source_file_path, target_file_path)
+
+
+def get_selected_tag_comment(selected_tag: str | None) -> str:
+    """
+    Generates description for a selected tag.
+    """
+
+    return (
+        "with no tag selected"
+        if selected_tag is None
+        else f'for "{selected_tag}" tag selected'
+    )
+
+
+def get_html_template_parameters(
+    metadata: metadata_reader.BlogMetadata,
+    page_title: str,
+    page_description: str,
+    page_path: str,
+    page_is_editable: bool,
+) -> dict[str, typing.Any]:
+    """
+    Returns common parameters for HTML templates.
+    """
+
+    page_edit_url = __get_page_edit_url(metadata, page_path, page_is_editable)
+
+    return {
+        "language": metadata.language,
+        "settings": metadata.settings,
+        "page_title": page_title,
+        "page_description": page_description,
+        "page_path": page_path,
+        "page_edit_url": page_edit_url,
+    }
+
+
+def __get_page_edit_url(
+    metadata: metadata_reader.BlogMetadata, page_path: str, page_is_editable: bool
+) -> str:
+    """
+    Determines if a page can be edited, then makes a link to GitHub.
+    """
+
+    result = ""
+
+    if page_is_editable and metadata.settings.get("github_repository"):
+        parts = [
+            f"https://github.com/{metadata.settings['github_repository']}/edit/main"
+        ]
+
+        if page_path:
+            parts.append(page_path)
+
+        parts.append("index.md")
+
+        result = "/".join(parts)
+
+    return result
