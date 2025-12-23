b9405f265ae3002a2f66bc906eb9aca69b566c16
Vlad Kostyanetsky <vkostyanetsky@firstbit.ae>
2025-12-23 22:09:24 +0700

    One more directions fix

--- bloget/writers/note_writer.py
+++ bloget/writers/note_writer.py
@@ -24,14 +24,14 @@ def write_notes(
 
     notes = page_writing_utils.get_notes(pages.notes)
 
-    for index, note in enumerate(notes):
-        previous_note = None if len(notes) - 1 == index else notes[index + 1]
+    for index, note in enumerate(notes):        
         next_note = None if index == 0 else notes[index - 1]
+        previous_note = None if index == len(notes) - 1 else notes[index + 1]
 
-        __write_note(note, previous_note, next_note, metadata)
+        _write_note(note, previous_note, next_note, metadata)
 
 
-def __write_note(
+def _write_note(
     note: page_reader.BlogPage,
     previous_note: page_reader.BlogPage | None,
     next_note: page_reader.BlogPage | None,
